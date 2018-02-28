50f0a4e3af9ae3ccc3cb119af4474c690f3655be
levon83 <34793238+levon83@users.noreply.github.com>
2018-03-01 00:31:59 +0400

    * Fix horizontal condition.

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -64,9 +64,9 @@ public class Point implements Comparable<Point> {
 
         if (this.compareTo(that) == 0) {
             return Double.NEGATIVE_INFINITY;
-        } else if (this.x == that.x) {
-            return +0.0;
         } else if (this.y == that.y) {
+            return +0.0;
+        } else if (this.x == that.x) {
             return Double.POSITIVE_INFINITY;
         }
 
